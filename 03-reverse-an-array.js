60af75a2721835062622870e7f4d2a2e122e8d56
Pierre <pierre.laffaille@outlook.fr>
2024-11-25 10:52:17 +0100

    third problem

---
+++ 03-reverse-an-array.js
@@ -0,0 +1,21 @@
+// User function Template for javascript
+/**
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+
+class Solution {
+	// Function to reverse the array.
+	reverseArray(arr) {
+		// your code here
+		/*const temp = []
+        for (let i = arr.length - 1; i >= 0; i--) {
+            temp.push(arr[i]);
+        }
+        
+        return temp; */
+
+		arr.reverse();
+		return arr;
+	}
+}
